0d78aadc17875549c678875057025855cfcc1a08
Satya089312 <satya089312@gmail.com>
2026-07-31 16:59:40 +0530

    Delete terraform/modules/api_gateway directory

--- terraform/modules/api_gateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_endpoint" {
-  description = "API Gateway endpoint"
-  value       = aws_apigatewayv2_api.this.api_endpoint
-}
-
-output "api_id" {
-  description = "API Gateway ID"
-  value       = aws_apigatewayv2_api.this.id
-}
