8907ca543fa3c023f38199b3b29c1deef05c033d
Satya089312 <satya089312@gmail.com>
2026-08-03 12:08:52 +0530

    Refactor IAM variables in variables.tf

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,16 +1,11 @@
-variable "role_name" {
-  description = "Name of the IAM role"
-  type        = string
+variable "policy_name" {
+  type = string
 }
 
-variable "managed_policy_arns" {
-  description = "List of managed IAM policies to attach to the role"
-  type        = list(string)
-  default     = []
+variable "role_name" {
+  type = string
 }
 
 variable "tags" {
-  description = "Tags to apply to IAM resources"
-  type        = map(string)
-  default     = {}
+  type = map(string)
 }
