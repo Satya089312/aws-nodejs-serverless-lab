21d89a4bc306025e646b030ad5613e914264e48a
Satya089312 <satya089312@gmail.com>
2026-08-03 12:09:11 +0530

    Add policy_arn output to IAM module
    Added output for IAM policy ARN.

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,9 +1,11 @@
 output "role_arn" {
-  description = "ARN of the IAM role"
-  value       = aws_iam_role.this.arn
+  value = aws_iam_role.this.arn
 }
 
 output "role_name" {
-  description = "Name of the IAM role"
-  value       = aws_iam_role.this.name
+  value = aws_iam_role.this.name
+}
+
+output "policy_arn" {
+  value = aws_iam_policy.this.arn
 }
